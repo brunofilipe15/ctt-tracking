a74566b0ce4fb0c1d9095c0d96e2185a960141a4
Bruno <bruno_filip_15@hotmail.com>
2020-12-12 00:23:27 +0000

    add selenium test

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Goodbye, World!")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+url = 'https://www.ctt.pt/feapl_2/app/open/cttexpresso/objectSearch/objectSearch.jspx'
+driver = webdriver.Chrome('./chromedriver')
+print(driver.title)
